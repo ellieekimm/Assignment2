59ccbfc508511254459f161e39469d5e20787f19
ellieekimm <115567471+ellieekimm@users.noreply.github.com>
2023-02-02 15:24:29 -0500

    Add files via upload

---
+++ EntryData.java
@@ -0,0 +1,13 @@
+package assn02;
+import java.util.Scanner;
+public class EntryData {
+    String _date;
+    String _time;
+    String _category;
+    float _fee;
+    int _quantity;
+    float _totalTime;
+    int _cost;
+    }
+
+
